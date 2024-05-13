3531ba7706f1d0b793ce682f858b4a5ea297d554
Jorge Castillo <jorge.castillo12@educa.madrid.org>
2024-05-13 20:40:49 +0200

    -Añadido las vistas -Cambios en Principal -Añadido las altas bajas modificacion -falta añadir los metodos y mejoras visuales
    -Jorge y Aimar

--- NoAuto/src/noAuto/principal/Principal.java
+++ NoAuto/src/noAuto/principal/Principal.java
@@ -1,8 +1,13 @@
 package noAuto.principal;
 
+
 public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Aupa Athletic");
+		
+		System.out.println("Arrancando la aplicacion  ....");
+		Menu menu = new Menu(); // LLamamos al menu al principal para que se visualice
+		menu.setVisible(true); // Esto es para decirle que se puede visualizar por eso el true
+
 	}
 
 }
---
+++ NoAuto/src/noAuto/vista/BajaClienteVista.java
@@ -0,0 +1,16 @@
+package noAuto.vista;
+
+import javax.swing.JPanel;
+
+public class BajaClienteVista extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public BajaClienteVista() {
+
+	}
+
+}
